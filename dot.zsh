268f0ef872e4140cf1a2704f853b8ec29f7ceb0b
Charles Miquelon <charles@miquelon.dev>
2020-09-29 00:13:06 -0400

    dot.sh -> dot.zsh

--- dot.zsh
+++ dot.zsh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env zsh
+
+dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 # avoid showing everthing in $HOME with git status
 dotfiles config --local status.showUntrackedFiles no
